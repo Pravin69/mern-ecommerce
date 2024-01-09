d2a9b7fbc2657261c245d41bc1cc7b8ebfcfbced
Pravin69 <p.thakur.133.ram@gmail.com>
2024-01-09 19:21:14 +0530

    Login bug fix

--- Client/src/features/auth/components/Login.jsx
+++ Client/src/features/auth/components/Login.jsx
@@ -13,6 +13,8 @@ export default function Login() {
     formState: { errors },
   } = useForm();
 
+  // console.log(error);
+
   return (
     <>
       {user && <Navigate to="/" replace={true}></Navigate>}
@@ -36,7 +38,6 @@ export default function Login() {
               dispatch(
                 loginUserAsync({ email: data.email, password: data.password })
               );
-              // console.log(data);
             })}
           >
             <div>
@@ -62,7 +63,7 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <p className="text-red-500">
-                  {errors.email && errors?.email?.message}
+                  {errors?.email && errors?.email?.message}
                 </p>
               </div>
             </div>
@@ -94,11 +95,12 @@ export default function Login() {
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
                 <p className="text-red-500">
-                  {errors.password && errors?.password?.message}
+                  {errors?.password && errors?.password?.message}
                 </p>
-
                 {error && (
-                  <p className="text-red-500">{error || error.message}</p>
+                  <p className="text-red-500">
+                    {error?.message || error?.error}
+                  </p>
                 )}
               </div>
             </div>
